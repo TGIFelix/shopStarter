b378350d97104351961709ad7367821279ba0d2d
Felix Swinkels <fswinkels@53gradennoord.nl>
2021-10-22 17:05:17 +0200

    swiper and lightbox

--- src/script.js
+++ src/script.js
@@ -1,3 +1,10 @@
 // IMPORT STYLES
 import './tailwind.css';
 import './main.scss';
+
+// SWIPERJS
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+// FSLIGHTBOX
+require('fslightbox');
